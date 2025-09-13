a0046caf441ef818977979c349b85de890f838c7
caio.imperial <scaioimperial@gmail.com>
2025-09-13 01:07:42 -0300

    Add initial frontend setup for Nuxt application with configuration files, .gitignore, and basic structure

---
+++ apps/front/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>{{ data?.message }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+const { data } = await useFetch<{ message: string }>('http://localhost:3001');
+</script>
