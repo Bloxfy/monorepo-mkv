ba64e2b7547751dbf4cb7080aa7d25675f0f8639
caio.imperial <scaioimperial@gmail.com>
2025-09-13 12:16:53 -0300

    feat(backend)(auth): Implement authentication module with JWT support, including login functionality and validation

--- apps/backend/src/app.controller.spec.ts
+++ apps/backend/src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 describe('AppController', () => {
   let appController: AppController;
