568384073a091d342e6fb80be9583db72a156652
caio.imperial <scaioimperial@gmail.com>
2025-09-13 01:07:34 -0300

    Add initial backend setup for NestJS application with configuration files

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+}
+bootstrap();
