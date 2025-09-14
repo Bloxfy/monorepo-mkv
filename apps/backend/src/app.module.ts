9514cc8605e6a1d33b536edd94a52cc502f95740
caio.imperial <scaioimperial@gmail.com>
2025-09-14 02:07:32 -0300

    feat(backend)(auth): Enhance JWT authentication with public route support and integrate Prisma and Users modules

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,17 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from './auth/jwt.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule
+    AuthModule,
+    UsersModule,
+    PrismaModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    }
+  ],
 })
 export class AppModule {}
