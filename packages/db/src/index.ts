91bb279c4bd8c92893074e1ac5d6fc954dd66212
caio.imperial <scaioimperial@gmail.com>
2025-09-14 00:23:27 -0300

    refactor(package)(db): Rearrange build script order, enable declaration maps, and migrate Prisma client setup to index.ts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,13 @@
-export * from './client';
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma: PrismaClient =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
