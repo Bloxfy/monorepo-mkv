723c78451a9937854950cdb709ed7da9bccb786f
caio.imperial <scaioimperial@gmail.com>
2025-09-18 00:14:09 -0300

    fix(backend)(wallets): Correct import paths for UsersModule and PrismaModule in wallets.module.ts

--- apps/backend/src/wallets/wallets.module.ts
+++ apps/backend/src/wallets/wallets.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
-import { UsersModule } from 'src/users/users.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [UsersModule, PrismaModule],
