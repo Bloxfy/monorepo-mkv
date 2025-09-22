9a5c98486e99b7d425b8a64de160d834a26c9aee
caio.imperial <scaioimperial@gmail.com>
2025-09-21 23:23:19 -0300

    feat(frontend)(utils): Add ApiError interface and getApiError function for error handling

--- apps/front/app/lib/utils.ts
+++ apps/front/app/lib/utils.ts
@@ -5,3 +5,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+import type { FetchResponse } from 'openapi-fetch'
+
+export interface ApiError {
+  statusCode: number
+  message: string
+  error: string
+}
+
+/**
+ * Retorna o erro do fetch ou null se for sucesso
+ */
+export function getApiError<T = ApiError>(
+  res: FetchResponse<any, any, any>
+): T | null {
+  if ('error' in res) {
+    return res.error as T
+  }
+  return null
+}
