dea41ed615621c3fd861c8ad52cdcc57000bc9aa
caio.imperial <scaioimperial@gmail.com>
2025-09-21 23:23:30 -0300

    feat(frontend)(login, sign-up): Refactor login and sign-up components for improved error handling and layout consistency

--- apps/front/app/pages/login.vue
+++ apps/front/app/pages/login.vue
@@ -1,56 +1,70 @@
 <template>
-    <div class="grid min-h-svh lg:grid-cols-2">
-        <div class="flex flex-col gap-4 p-6 md:p-10">
-            <div class="flex justify-between items-center gap-2">
-                <a href="#" class="flex items-center gap-2 font-medium">
-                    <div class="flex h-12 w-auto items-center justify-center rounded-md text-primary-foreground">
-                        <img src="~/assets/images/logo.png" alt="Mockey Invest" class="h-full w-auto" />
-                    </div>
-                    Mockey Invest
-                </a>
-                <ThemeSwitch />
-            </div>
-            <div class="flex flex-1 items-center justify-center">
-                <div class="w-full max-w-xs">
-                    <FormLogin ref="loginRef" @submit="handleSubmit" :is-loading="isLoading" />
-                </div>
-            </div>
+  <div class="grid min-h-svh lg:grid-cols-2">
+    <div class="flex flex-col gap-4 p-6 md:p-10">
+      <div class="flex justify-between items-center gap-2">
+        <a href="#" class="flex items-center gap-2 font-medium">
+          <div class="flex h-12 w-auto items-center justify-center rounded-md text-primary-foreground">
+            <img src="~/assets/images/logo.png" alt="Mockey Invest" class="h-full w-auto" />
+          </div>
+          Mockey Invest
+        </a>
+        <ThemeSwitch />
+      </div>
+      <div class="flex flex-1 items-center justify-center">
+        <div class="w-full max-w-xs">
+          <FormLogin ref="loginRef" @submit="handleSubmit" :is-loading="isLoading" />
         </div>
-        <div class="relative hidden bg-muted lg:block">
-            <img src="" alt="Image"
-                class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale">
-        </div>
+      </div>
     </div>
+    <div class="relative hidden bg-muted lg:block">
+      <img src="" alt="Image" class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale">
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import FormLogin from '~/components/Form/Login.vue'
+import { getApiError } from '~/lib/utils'
 
+definePageMeta({
+  layout: 'default-public'
+})
+
+const { $api } = useNuxtApp()
 const loginRef = ref<InstanceType<typeof FormLogin>>()
 const isLoading = ref(false)
 
-const handleSubmit = async (values: { email: string, password: string }) => {
-    isLoading.value = true
-    await $fetch<{ accessToken: string }>('http://localhost:3001/users/login', {
-        method: 'POST',
-        body: {
-            username: values.email,
-            password: values.password
-        },
-        credentials: 'include'
-    }).then((res) => {
-        if (res.accessToken) {
-            useCookie('token').value = res.accessToken
-            navigateTo('/dashboard')
-        }
-    }).catch((err) => {
-        console.log(err)
-        if (loginRef.value && err.statusCode === 401) {
-            loginRef.value.setErrors({
-                email: err.data.message,
-                password: err.data.message
-            })
-        }
+const handleSubmit = async (values: { email: string; password: string }) => {
+  isLoading.value = true
+
+  try {
+    const res = await $api.POST('/users/login', {
+      method: 'POST',
+      body: {
+        username: values.email,
+        password: values.password,
+      },
+      credentials: 'include',
     })
+
+    const error = getApiError(res)
+
+    if (error && error.statusCode === 401 && loginRef.value) {
+      loginRef.value.setErrors({
+        email: error.message,
+        password: error.message,
+      })
+      return
+    }
+
+    // se não houve erro, é sucesso
+    if ('data' in res && res.data?.accessToken) {
+      useCookie('token').value = res.data.accessToken
+      navigateTo('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+  } finally {
     isLoading.value = false
+  }
 }
 </script>
--- apps/front/app/pages/sign-up.vue
+++ apps/front/app/pages/sign-up.vue
@@ -1,57 +1,68 @@
 <template>
-    <div class="flex flex-col h-screen">
-        <div class="flex justify-between items-center gap-2 p-6 md:p-10">
-            <a href="#" class="flex items-center gap-2 font-medium">
-                <div class="flex h-12 w-auto items-center justify-center rounded-md text-primary-foreground">
-                    <img src="~/assets/images/logo.png" alt="Mockey Invest" class="h-full w-auto" />
-                </div>
-                Mockey Invest
-            </a>
-            <ThemeSwitch />
+  <div class="flex flex-col h-screen">
+    <div class="flex justify-between items-center gap-2 p-6 md:p-10">
+      <a href="#" class="flex items-center gap-2 font-medium">
+        <div class="flex h-12 w-auto items-center justify-center rounded-md text-primary-foreground">
+          <img src="~/assets/images/logo.png" alt="Mockey Invest" class="h-full w-auto" />
         </div>
-        <div class="flex flex-grow items-center justify-center">
-            <div class="w-full max-w-xs">
-                <FormSignUp ref="signUpRef" @onSubmit="handleSubmit" :error-validate="errorValidate"
-                    :is-loading="isLoading" />
-            </div>
-        </div>
+        Mockey Invest
+      </a>
+      <ThemeSwitch />
     </div>
+    <div class="flex flex-grow items-center justify-center">
+      <div class="w-full max-w-xs">
+        <FormSignUp ref="signUpRef" @onSubmit="handleSubmit" :is-loading="isLoading" />
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-import FormSignUp from '~/components/Form/SignUp.vue'
-
+import FormSignUp from '@/components/Form/SignUp.vue'
+import { getApiError } from '@/lib/utils'
+definePageMeta({
+  layout: 'default-public'
+})
+const { $api } = useNuxtApp()
 const signUpRef = ref<InstanceType<typeof FormSignUp>>()
 const isLoading = ref(false)
 
-const errorValidate = ref({
-    email: {
-        message: ''
-    }
-})
-
 const handleSubmit = async (values: {
-    email: string
-    password: string
-    name: string
-    lastName: string
+  email: string
+  password: string
+  name: string
+  lastName: string
 }) => {
-    isLoading.value = true
-    await $fetch('http://localhost:3001/users/signup', {
-        method: 'POST',
-        body: {
-            email: values.email,
-            password: values.password,
-            name: values.name,
-            lastName: values.lastName
-        }
-    }).then((res) => {
-        console.log(res)
-    }).catch(async (err) => {
-        if (err.status === 409) {
-            console.log('error')
-            signUpRef.value?.setErrors({ email: 'Email already exists' })
-        }
+  isLoading.value = true
+
+  try {
+    const res = await $api.POST('/users/signup', {
+      method: 'POST',
+      body: {
+        email: values.email,
+        password: values.password,
+        name: values.name,
+        lastName: values.lastName
+      }
     })
+
+    const error = getApiError(res)
+
+    if (error && error.statusCode === 409 && signUpRef.value) {
+      signUpRef.value.setErrors({
+        email: error.message
+      })
+      return
+    }
+
+    if ('data' in res && res.data?.accessToken) {
+      useCookie('token').value = res.data.accessToken
+      navigateTo('/dashboard')
+    }
+  } catch (error) {
+    console.error(error)
+  }
+  finally {
     isLoading.value = false
+  }
 }
 </script>
